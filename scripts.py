4205b902841a11405c2f00c0c6f9edf5d6e41ab8
TorbenOestergaard <torbeniha@gmail.com>
2021-08-27 09:56:14 +0200

    Added imports to scripts

--- scripts.py
+++ scripts.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+
+
 def transform_categorical_features(XY_raw, verbose=True):
   """
   Transforms categorical features to numerical using ordinal encoder.
